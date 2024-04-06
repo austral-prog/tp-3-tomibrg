69635e43c67935c36a3a0b77fed160571a2445be
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-04-06 10:03:22 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,3 +3,6 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    texto = input('Ingrese una palabra o frase:')
+    print(texto[4::2])
+slice_advanced()
