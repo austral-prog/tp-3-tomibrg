0ea4fb2f6e734aaddfdc071231a45d38a65b4e38
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-04-06 09:33:25 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -6,3 +6,10 @@ def slice_simple():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    texto = texto.lower()
+    lim_1 = len(texto)*1/2-1
+    lim_2 = len(texto)*1/2+2 
+    print(texto[:3])
+    print(texto[int(lim_1):int(lim_2)])
+    print(texto[:4] + texto[-3:])
+slice_simple()
